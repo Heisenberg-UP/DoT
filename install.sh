ec454b3c659047a58a14cbb129db8d226b1e63cf
Dr. Under-Qualified <99283516+Heisenberg-UP@users.noreply.github.com>
2024-03-31 21:33:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ user=$(whoami)
 
 # Install necessary packages
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install ngrep unbound -y
+sudo apt-get install ngrep unbound iptables-persistent -y
 
 # Move vanilla resolved configuration file to DoT folder
 sudo cp /etc/unbound/unbound.conf $dir/DoT-AdBlock/files/unbound.conf.original
@@ -15,5 +15,23 @@ chmod 400 $dir/DoT-AdBlock/files/resolved.conf.original
 
 # Copy custom resolved configuation file to /etc/systemd/ folder
 sudo cp $dir/DoT-AdBlock/files/unbound.conf /etc/unbound/unbound.conf
+sudo chown root:root /etc/unbound/unbound.conf
 sudo chmod 644 /etc/unbound/unbound.conf
+
+# Make symbolic link for logrotate file
+sudo ln -s $dir/DoT-AdBlock/files/logrotate-unbound /etc/logrotate.d/logrotate-unbound
+sudo chown root:root $dir/DoT-AdBlock/files/logrotate-unbound
+sudo chmod 644 $dir/DoT-AdBlock/files/logrotate-unbound
+
+# Add AppArmor file
+sudo cp $dir/DoT-AdBlock/files/usr.sbin.unbound /etc/apparmor.d/local/usr.sbin.unbound
+sudo chown root:root /etc/apparmor.d/local/usr.sbin.unbound
+sudo chmod 644 /etc/apparmor.d/local/usr.sbin.unbound
+
+# Test Unbound without firewall
+sudo iptables -F
+sudo systemctl try-restart unbound
+
+
+
 
